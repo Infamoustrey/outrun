575069ca5be4f12236f579308e14f915c093846d
infamoustrey <treybfoster@gmail.com>
2017-11-18 23:38:26 -0600

    Init Commit

---
+++ game.js
@@ -0,0 +1,20 @@
+let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+
+function preload() {
+
+
+    game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+
+}
+
+function create() {
+
+}
+
+
+function update() {
+
+}
